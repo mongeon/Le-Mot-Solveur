019b147eeac24776a34f9435b491141b3e42b9d4
Gabriel <mongeon@users.noreply.github.com>
2022-04-05 16:38:12 -0400

    Add minimal API and some FE

---
+++ Client/Pages/Index.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Net.Http.Json;
+using System.Text.RegularExpressions;
+
+namespace LeMotSolveur.Client.Pages;
+
+public partial class Index
+{
+    [Inject]
+    public HttpClient http { get; set; }
+    public string PossibleCharacters { get; set; }
+
+    
+    public IEnumerable<string> allWords = new List<string>();
+    public IEnumerable<string> words = new List<string>();
+
+    protected override async Task OnInitializedAsync()
+    {
+
+        allWords = await http.GetFromJsonAsync<IEnumerable<string>>("api/words");
+        words = allWords;
+    }
+    public async void Search()
+    {
+        PossibleCharacters = PossibleCharacters.ToUpperInvariant();
+        var results = string.IsNullOrEmpty(PossibleCharacters) || PossibleCharacters.Equals("*") ?
+                               allWords :
+                               allWords.ToList().FindAll((string s) => Regex.IsMatch(s, "^[" + PossibleCharacters + "]{5}$"));
+        words = results;
+    }
+}
