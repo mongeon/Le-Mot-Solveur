13cf6768aabb2308ec557eb61d143e4957b37170
Gabriel <mongeon@users.noreply.github.com>
2022-04-06 22:16:48 -0400

    Added the Solver api

---
+++ Api/FindWords.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace LeMotSolveur.Api;
+
+public class FindWords
+{
+    private readonly IEnumerable<string> _words;
+
+    public FindWords(IEnumerable<string> words)
+    {
+        _words = words;
+    }
+    public IEnumerable<string> Process(string fixedLetters, string goodLetters, string badLetters)
+    {
+        return Process(fixedLetters.ToCharArray(), goodLetters.ToCharArray(), badLetters.ToCharArray());
+    }
+    public IEnumerable<string> Process(char[] fixedLetters, char[] goodLetters, char[] badLetters)
+    {
+        var regexExpression = "";
+
+        foreach (var goodLetter in goodLetters)
+        {
+            regexExpression += $"(?=[A-Z]*{goodLetter.ToString().ToUpperInvariant()})";
+        }
+        if (badLetters.Length > 0)
+        {
+            regexExpression += $"(?![A-Z]*[{ string.Concat(badLetters).ToUpperInvariant()}])";
+        }
+
+        regexExpression = $"^{regexExpression}[A-Z]{{5}}$";
+        var results = _words.ToList().FindAll((string s) => Regex.IsMatch(s, regexExpression));
+
+        if (fixedLetters.Length == 5)
+        {
+            regexExpression = string
+                .Concat(fixedLetters)
+                .ToUpperInvariant()
+                .Replace("*", "[A-Z]{1}")
+                .Replace(" ", "[A-Z]{1}")
+                .Replace("_", "[A-Z]{1}");
+
+            regexExpression = $"^{regexExpression}$";
+            results = results.ToList().FindAll((string s) => Regex.IsMatch(s, regexExpression));
+        }
+
+        return results;
+    }
+
+}
--- Api/Solveur.cs
+++ Api/Solveur.cs
@@ -1,5 +1,9 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Net;
+using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -21,19 +25,71 @@ public class Solveur
         _logger = log;
     }
 
-    [FunctionName("words")]
+    [FunctionName("GetAllWords")]
     [OpenApiOperation(operationId: "Run")]
     [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
     [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(string), Description = "The OK response")]
     public async Task<IActionResult> Run(
-        [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req, ExecutionContext context)
+        [HttpTrigger(AuthorizationLevel.Function, "get", Route = "words")] HttpRequest req, ExecutionContext context)
     {
-         _logger.LogInformation("C# HTTP trigger function processed a request.");
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
 
-        var path = Path.Combine(context.FunctionAppDirectory, "Assets", "drawable-words.json");
+        string path = GetAllWords(context);
         var words = await File.ReadAllTextAsync(path);
 
         return new OkObjectResult(words);
     }
-}
 
+    [FunctionName("ResolveWords")]
+    [OpenApiOperation(operationId: "Resolve")]
+    [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+    [OpenApiRequestBody(contentType: "application/json", bodyType: typeof(Parameters), Required = true, Description = "Parameters")]
+    [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "application/json", bodyType: typeof(string), Description = "The OK response")]
+    public async Task<IActionResult> Resolve(
+        [HttpTrigger(AuthorizationLevel.Function, "post", Route = "words")] HttpRequest req, ExecutionContext context)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        if (req.Body != null && req.ContentLength > 0)
+        {
+            string path = GetAllWords(context);
+            var fileContent = await File.ReadAllTextAsync(path);
+
+            var allWords = JsonSerializer.Deserialize<List<string>>(fileContent);
+            var solver = new FindWords(allWords);
+            // read the contents of the posted data into a string
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+
+            // use Json.NET to deserialize the posted JSON into a C# dynamic object
+            var data = JsonSerializer.Deserialize<Parameters>(requestBody, new JsonSerializerOptions() { PropertyNameCaseInsensitive = true });
+            var words = solver.Process(data.FixedLetters, data.GoodLetters, data.BadLetters);
+
+            return new OkObjectResult(words);
+        }
+        return new BadRequestObjectResult("Body of request is invalid");
+    }
+
+    private static string GetAllWords(ExecutionContext context)
+    {
+        return Path.Combine(context.FunctionAppDirectory, "Assets", "drawable-words.json");
+    }
+
+
+    //[OpenApiExample(typeof(Parameters))]
+    public class Parameters
+    {
+        [JsonPropertyName("fixedLetters")]
+        [Required(AllowEmptyStrings = true)]
+        public string FixedLetters { get; set; }
+        [JsonPropertyName("goodLetters")]
+        [Required(AllowEmptyStrings = true)]
+        public string GoodLetters { get; set; }
+
+        /// <summary>
+        /// List of all letters that are not in the word
+        /// </summary>
+        [JsonPropertyName("badLetters")]
+        [Required(AllowEmptyStrings = true)]
+        public string BadLetters { get; set; }
+    }
+}
