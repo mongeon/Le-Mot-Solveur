c0c6bb8aff1979854f792d8092d4baf565fd542e
Gabriel <mongeon@users.noreply.github.com>
2022-04-18 10:31:37 -0400

    Added the Solver api (#9)
    * Added the Solver api
    
    * Updated the UI to use new API
    
    * Always force fixed letters

--- Client/Pages/Index.razor.cs
+++ Client/Pages/Index.razor.cs
@@ -9,23 +9,41 @@ public partial class Index
     [Inject]
     public HttpClient http { get; set; }
     public string PossibleCharacters { get; set; }
-
+    public string ExactCharacters { get; set; }
+    public string BadCharacters { get; set; }
     
+
     public IEnumerable<string> allWords = new List<string>();
     public IEnumerable<string> words = new List<string>();
+    public bool IsSearching = false;
 
     protected override async Task OnInitializedAsync()
     {
-
+        IsSearching = true;
         allWords = await http.GetFromJsonAsync<IEnumerable<string>>("api/words");
         words = allWords;
+        IsSearching = false;
     }
     public async void Search()
     {
-        PossibleCharacters = PossibleCharacters.ToUpperInvariant();
-        var results = string.IsNullOrEmpty(PossibleCharacters) || PossibleCharacters.Equals("*") ?
-                               allWords :
-                               allWords.ToList().FindAll((string s) => Regex.IsMatch(s, "^[" + PossibleCharacters + "]{5}$"));
-        words = results;
+        IsSearching = true;
+        var request = new
+        {
+            fixedLetters = ExactCharacters?.ToUpperInvariant(),
+            goodLetters = PossibleCharacters?.ToUpperInvariant(),
+            badLetters = BadCharacters?.ToUpperInvariant(),
+        };
+        var result = await http.PostAsJsonAsync("api/words", request);
+        if (result.IsSuccessStatusCode)
+        {
+            
+            words = await result.Content.ReadFromJsonAsync<IEnumerable<string>>();
+        }
+        else
+        {
+            words = new List<string>();
+        }        
+        IsSearching = false;
+        base.StateHasChanged();
     }
 }
